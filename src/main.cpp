7b39df982539722ea09ca259cd99b2c173e7f9cf
trabus322 <trabus322.main@gmail.com>
2024-03-20 19:25:00 +0100

    Commit to clean up and test CI

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,8 @@ void sigHandler(int signal);
 
 int main(int argc, char **argv) {
 	signal(SIGINT, sigHandler);
-	// signal(SIGBREAK, &handler);
 	signal(SIGTERM, sigHandler);
 	signal(SIGABRT, sigHandler);
-	// signal(SIGQUIT, sigHandler);
 
 	std::string version{"v1.0"};
 	std::cout << version << std::endl;
